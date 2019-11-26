6531b7e330a77b55b6cc9be5f63804091859548c
kim min ji <216000069@handong.eud>
2019-11-26 23:52:40 +0900

    Create CH10_Grader_main.java

---
+++ CH10/src/CH10_Grader_main.java
@@ -0,0 +1,9 @@
+
+public class CH10_Grader_main {
+	public static void main(String[] args) {
+		String fileName = "StudentGrades.txt";
+		String outfileName = "StudentGrades_average.txt";
+		CH10_Grader.run(fileName,outfileName);
+	}
+
+}
