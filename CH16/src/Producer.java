ce278f154520b8227e6504d72af57704e9b08fe5
kim min ji <216000069@handong.eud>
2019-12-17 15:36:09 +0900

    Create Producer.java

---
+++ CH16/src/Producer.java
@@ -0,0 +1,20 @@
+
+public class Producer extends Thread {
+	protected BoundedStack stack; 
+	protected int n;
+	
+	public Producer(BoundedStack stack, int n) { 
+		this.stack = stack;
+		this.n = n; 
+	}
+	
+	public void run() {
+		for (int i = 0; i < n; i++) {
+			stack.put(new Integer(i));
+			System.out.println("push: " + i);
+			try {
+				sleep((int)(Math.random() * 100)); 
+				} catch (InterruptedException e) {}
+			}
+		}
+}
