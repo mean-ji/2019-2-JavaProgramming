af9035afd91bff5b3ab8f7283cd140b95f15dcf0
kim min ji <216000069@handong.eud>
2019-12-17 15:37:21 +0900

    Create CH13_KeypadWIndowMain.java

---
+++ CH13/src/CH13_KeypadWIndowMain.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CH13_KeypadWIndowMain{
+	public static void main(String[] args) { 
+		CH13_KeypadWindow window1 = new CH13_KeypadWindow();
+		window1.setVisible(true);
+	}
+}
