71afe8708d0e85030ae828977fe5d89a62d725e6
kim min ji <216000069@handong.eud>
2019-12-17 15:36:51 +0900

    Create CH15_SundaeCreatorMain.java

---
+++ CH15/src/CH15_SundaeCreatorMain.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CH15_SundaeCreatorMain {
+	public static void main(String[] args) {
+		CH15_SundaeCreator window1 = new CH15_SundaeCreator();
+		window1.setVisible(true);
+	}
+
+}
