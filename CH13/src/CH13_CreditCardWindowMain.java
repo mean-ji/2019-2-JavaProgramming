5e448dbc5b1843e68f5bb41670bc06103c32397f
kim min ji <216000069@handong.eud>
2019-12-17 15:37:26 +0900

    Create CH13_CreditCardWindowMain.java

---
+++ CH13/src/CH13_CreditCardWindowMain.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class CH13_CreditCardWindowMain {
+	public static void main(String[] args) {
+		CH13_CreditCardWindow window1 = new CH13_CreditCardWindow();
+		window1.setVisible(true);
+	}
+}
