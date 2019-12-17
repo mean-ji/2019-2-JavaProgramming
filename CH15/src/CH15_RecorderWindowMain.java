9901ed1ee7087884d3b911987f5bee2e33d0773b
kim min ji <216000069@handong.eud>
2019-12-17 15:36:56 +0900

    Create CH15_RecorderWindowMain.java

---
+++ CH15/src/CH15_RecorderWindowMain.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CH15_RecorderWindowMain {
+	public static void main(String[] args) { 
+		CH15_RecorderWindow window1 = new CH15_RecorderWindow(); 
+		window1.setVisible(true);
+	}
+}
