36af6ab38d23a62950168320390c32e3a195ca8a
kim min ji <216000069@handong.eud>
2019-12-17 15:35:46 +0900

    Create swingmain.java

---
+++ Report04/src/swingmain.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class swingmain {
+	public static void main(String[] args) {
+		swing window1 = new swing();
+		window1.setVisible(true);
+	}
+
+}
+
